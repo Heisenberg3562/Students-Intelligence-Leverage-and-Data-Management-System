dcf4ba4aa701598b303631756f2c47ebad72ff30
Heisenberg3562 <babrekaromkar@gmail.com>
2021-08-12 17:23:46 +0530

    Update head.blade.php

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -3,6 +3,7 @@
 <meta name="description" content="">
 <meta name="keywords" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 <link rel="icon" href="{{ asset('favicon.png')}}" />
 
