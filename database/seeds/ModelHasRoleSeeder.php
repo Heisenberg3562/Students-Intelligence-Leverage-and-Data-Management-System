6e8123cf28518d604f41035eff473eb7a3fe9944
Heisenberg3562 <babrekaromkar@gmail.com>
2021-08-15 20:07:08 +0530

    Seeder

--- database/seeds/ModelHasRoleSeeder.php
+++ database/seeds/ModelHasRoleSeeder.php
@@ -17,22 +17,22 @@ class ModelHasRoleSeeder extends Seeder
         DB::table('model_has_roles')->insert([
             [
                 'role_id' => '1',
-                'model_type' => 'App\user',
+                'model_type' => 'App\User',
                 'model_id' => '1'
             ],
             [
                 'role_id' => '2',
-                'model_type' => 'App\user',
+                'model_type' => 'App\User',
                 'model_id' => '4'
             ],
             [
                 'role_id' => '3',
-                'model_type' => 'App\user',
+                'model_type' => 'App\User',
                 'model_id' => '2'
             ],
             [
                 'role_id' => '4',
-                'model_type' => 'App\user',
+                'model_type' => 'App\User',
                 'model_id' => '3'
             ]
         ]);
