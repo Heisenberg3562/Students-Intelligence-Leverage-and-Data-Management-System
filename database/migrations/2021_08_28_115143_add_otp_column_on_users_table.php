05bb114640ac6797f2bd5cc75fcec1c8bd148c05
Heisenberg3562 <babrekaromkar@gmail.com>
2021-10-30 00:39:46 +0530

    Home page

--- database/migrations/2021_08_28_115143_add_otp_column_on_users_table.php
+++ database/migrations/2021_08_28_115143_add_otp_column_on_users_table.php
@@ -14,7 +14,7 @@ class AddOtpColumnOnUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('otp')->nullable();
+            $table->integer('otp')->default(0);
         });
     }
 
